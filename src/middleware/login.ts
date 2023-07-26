7946e52ccfe76d1ce190108e93f9fe9e65eb330a
Davidrami12 <davidrami2000@gmail.com>
2023-07-26 16:50:00 +0200

    Fix error on auth token

--- src/middleware/login.ts
+++ src/middleware/login.ts
@@ -1,19 +1,31 @@
-import { Request, Response, NextFunction } from "express";
+import { Request as ExpressRequest, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 
-export function authenticateToken(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
+interface Request extends ExpressRequest {
+  user?: any;
+}
 
-  if (token == null) return res.sendStatus(401)
+export function authenticateToken(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    return res.status(401).json({
+      auth: false,
+      message: 'No token provided'
+    });
+  }
 
   jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
-    console.log(err)
+    if (err) {
+      console.log(err);
+      return res.status(403).json({
+        auth: false,
+        message: 'Token is not valid'
+      });
+    }
 
-    if (err) return res.sendStatus(403)
-
-    req.user = user
-
-    next()
-  })
+    req.user = user;
+    next();
+  });
 }
