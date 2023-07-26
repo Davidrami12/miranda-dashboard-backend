45c0ca584dfb71d5ad0bfccbb8d90568404aa25a
Davidrami12 <davidrami2000@gmail.com>
2023-07-26 16:48:30 +0200

    Update login.ts

--- src/middleware/login.ts
+++ src/middleware/login.ts
@@ -1,31 +1,19 @@
-import { Request as ExpressRequest, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 
-interface Request extends ExpressRequest {
-  user?: any;
-}
-
 export function authenticateToken(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
 
-  if (token == null) {
-    return res.status(401).json({
-      auth: false,
-      message: 'No token provided'
-    });
-  }
+  if (token == null) return res.sendStatus(401)
 
   jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
-    if (err) {
-      console.log(err);
-      return res.status(403).json({
-        auth: false,
-        message: 'Token is not valid'
-      });
-    }
+    console.log(err)
 
-    req.user = user;
-    next();
-  });
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
 }
