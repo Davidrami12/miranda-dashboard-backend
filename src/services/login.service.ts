e725f7f238d5edc428308b363e506d62d42a5cc8
Davidrami12 <davidrami2000@gmail.com>
2023-08-16 19:53:26 +0200

    Encrypt passwords with bcrypt for security

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import uuid from "react-uuid";
 import jwt from "jsonwebtoken";
+import { encryptPassword, comparePassword } from "../utils/utils";
 
 export const loginService = async (req: express.Request, res: express.Response) => {
   const user = {
@@ -11,13 +12,17 @@ export const loginService = async (req: express.Request, res: express.Response) 
   const { email, password } = req.body;
   console.log(email, password)
 
+  // Hash the hardcoded password for comparison
+  const hashedPassword = await encryptPassword(user.password);
+
+  
   if ((email === "" || password === "")) {
     res.status(400).json({ error: "Missing user credentials" });
 
-  } else if (user.email !== email || user.password !== password) {
+  }else if (user.email !== email || !(await comparePassword(password, hashedPassword))) {
     res.status(401).json({ error: "User credentials don't match" });
-
-  } else{
+  
+  }else{
     const token = jwt.sign({ ...user, id: uuid() }, process.env.TOKEN_SECRET as string);
     res.status(200).json({ auth: true, token });
   }
